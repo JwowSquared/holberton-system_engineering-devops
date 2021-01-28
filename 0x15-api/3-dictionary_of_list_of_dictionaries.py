8e4fd9c20a0c873e5f8d198f2fecef3255835e48
JwowSquared <Jdwagner1@hotmail.com>
2021-01-28 21:24:09 +0000

    fixed??

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,18 +7,19 @@ if __name__ == "__main__":
     users = requests.get("http://jsonplaceholder.typicode.com/users").json()
     todos = requests.get("http://jsonplaceholder.typicode.com/todos").json()
     out = {}
+    tasks = []
+
+    for t in todos:
+        tasks.append([t.get("userId"), t.get("title"), t.get("completed")])
 
     for user in users:
-        username = user.get("username")
+        uname = user.get("username")
         target = user.get("id")
-        tasks = {}
-        for task in todos:
-            if task.get("id") == target:
-                tasks.update({task.get("title"): task.get("completed")})
-        emp = []
-        for t, c in tasks.items():
-            emp.append({"task": t, "completed": c, "username": username})
-        out.update({str(target): emp})
+        em = []
+        for t in tasks:
+            if target == t[0]:
+                em.append({"task": t[1], "completed": t[2], "username": uname})
+        out.update({str(target): em})
 
     with open("todo_all_employees.json", "w") as f:
         json.dump(out, f)
