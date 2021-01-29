463f82dc76748952b0e15ea3e7f737b166e2b17a
JwowSquared <Jdwagner1@hotmail.com>
2021-01-29 19:49:49 +0000

    checks if request returned anything

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,10 @@ def top_ten(subreddit):
     """asks reddit nicely for some info"""
     h = {"User-Agent": "Reddit Scraper"}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10"
-    raw = requests.get(url.format(subreddit), headers=h, allow_redirects=False)
+    raw = requests.get(url.format(subreddit), headers=h)
+    if not raw:
+        print(None)
+        return
     out = raw.json().get("data").get("children")
     if out:
         for hot in out:
