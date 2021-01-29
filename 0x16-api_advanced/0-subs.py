3aba7a074bc5241667d48a589b3a5b915fe99cb8
JwowSquared <Jdwagner1@hotmail.com>
2021-01-29 19:48:43 +0000

    removed the redirects thing because i dont think it matters

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """asks reddit nicely for some info"""
     h = {"User-Agent": "Reddit Scraper"}
     url = "https://www.reddit.com/r/{}/about.json"
-    raw = requests.get(url.format(subreddit), headers=h, allow_redirects=False)
+    raw = requests.get(url.format(subreddit), headers=h)
     if not raw:
         return 0
     out = raw.json().get("data").get("subscribers")
