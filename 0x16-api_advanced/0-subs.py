95105dbe195307a9324ec715441e5f2c4ff26ee9
JwowSquared <Jdwagner1@hotmail.com>
2021-01-29 19:46:16 +0000

    no longer uses default headers or allows redirects

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """asks reddit nicely for some info"""
-    headers = requests.utils.default_headers()
-    headers.update({"User-Agent": "Reddit Scraper"})
+    h = {"User-Agent": "Reddit Scraper"}
     url = "https://www.reddit.com/r/{}/about.json"
-    raw = requests.get(url.format(subreddit), headers=headers).json()
-    out = raw.get("data").get("subscribers")
+    raw = requests.get(url.format(subreddit), headers=h, allow_redirects=False)
+    out = raw.json().get("data").get("subscribers")
     if out:
         return out
     return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,11 +5,10 @@ import requests
 
 def top_ten(subreddit):
     """asks reddit nicely for some info"""
-    headers = requests.utils.default_headers()
-    headers.update({"User-Agent": "Reddit Scraper"})
+    h = {"User-Agent": "Reddit Scraper"}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10"
-    raw = requests.get(url.format(subreddit), headers=headers).json()
-    out = raw.get("data").get("children")
+    raw = requests.get(url.format(subreddit), headers=h, allow_redirects=False)
+    out = raw.json().get("data").get("children")
     if out:
         for hot in out:
             print(hot.get("data").get("title"))
