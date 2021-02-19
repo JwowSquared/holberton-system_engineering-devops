3926e5abdc5138ac1725283890f9cde9f7f49d89
JwowSquared <Jdwagner1@hotmail.com>
2021-02-19 14:15:13 +0000

    added comma on the end

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # script that fixes the typo that was breaking apache
 exec { 'fix':
     command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-    provider => "shell"
+    provider => "shell",
 }
